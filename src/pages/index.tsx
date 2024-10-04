27bb11c16ac8e8d9a093ef0b41872ef0a63f429e
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-10-03 20:44:25 -0400

    Format edit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,19 +8,20 @@
 // import ProfileProps from './components/profile_props'
 // //import Gallery from './qcomps/gallery_props'
 // import PackingList from './qcomps/props_item'
-import List from './qcomps/artistsRemoveArr'
+//import List from './qcomps/artistsRemoveArr'
 // import List from './qcomps/list_keys_id'
 // import RecipeList from './qcomps/recipes'
-//import Gallery from './qcomps/state'
-import Form from './components/updObjects'
-import ThankYouForm from './qcomps/thankYouCrash'
-import Counter from './components/queueUpdates'
-import RequestTracker from './qcomps/shoppingCart'
-import Scoreboard from './qcomps/updObjectsForm'
-import BucketList from './qcomps/arrObj'
+// import Gallery from './qcomps/state'
+// import Form from './qcomps/stuckForm'
+// import ThankYouForm from './qcomps/thankYouCrash'
+// import Counter from './components/queueUpdates'
+// import RequestTracker from './qcomps/shoppingCart'
+// import Scoreboard from './qcomps/updObjectsForm'
+// import BucketList from './qcomps/arrObj'
+import FeedbackForm from './qcomps/thankYouCrash'
 
 export default function Home() {
   return (
-   <BucketList />
+   <FeedbackForm />
   )
 }
